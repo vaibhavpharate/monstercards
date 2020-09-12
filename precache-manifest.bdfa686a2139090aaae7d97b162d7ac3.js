4eecc7c5e4acbe96e667c84bccdd830e7f02e00b
vaibhavpharate <vaibhavpharate999@gmail.com>
2020-09-12 16:44:19 +0530

    Updates

---
+++ precache-manifest.bdfa686a2139090aaae7d97b162d7ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f075b0ea791435434178a607c2f4ae8f",
+    "url": "/monstercards/index.html"
+  },
+  {
+    "revision": "0f5ddb36d6d0f09cea83",
+    "url": "/monstercards/static/css/main.bd618e78.chunk.css"
+  },
+  {
+    "revision": "25952efacaee3460870a",
+    "url": "/monstercards/static/js/2.12769881.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstercards/static/js/2.12769881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5ddb36d6d0f09cea83",
+    "url": "/monstercards/static/js/main.b2793868.chunk.js"
+  },
+  {
+    "revision": "c6760a622b2a73760cd3",
+    "url": "/monstercards/static/js/runtime-main.7463de9e.js"
+  }
+]);
